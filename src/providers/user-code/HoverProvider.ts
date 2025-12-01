9d2e1b82ccd649929ec160093ba7a65e4fee2362
Bux42 <roszwoka2@gmail.com>
2025-12-01 17:36:23 +0100

    Handle hover on nested class members

--- src/providers/user-code/HoverProvider.ts
+++ src/providers/user-code/HoverProvider.ts
@@ -1,15 +1,12 @@
 import * as vscode from "vscode";
-import * as path from "path";
 import { DefinitionManager } from "./DefinitionManager";
 import {
   UserClass,
   UserFunction,
   UserVariable,
 } from "../../services/analyzer/definitions.types";
-import {
-  getDefinitionAbsolutePath,
-  getMarkdownGoToDefinitionCommand,
-} from "../../utils/DefinitionUtils";
+import { getMarkdownGoToDefinitionCommand } from "../../utils/DefinitionUtils";
+import { getMemberAccessStringAtCursor } from "../../utils/UserCodeUtils";
 
 export class UserCodeHoverProvider implements vscode.HoverProvider {
   private definitionProvider: DefinitionManager;
@@ -26,17 +23,20 @@ export class UserCodeHoverProvider implements vscode.HoverProvider {
     const range = document.getWordRangeAtPosition(position);
     const word = document.getText(range);
 
-    console.log(`Hover requested for user code symbol: ${word}`);
+    const memberAccessStringAtCursor = getMemberAccessStringAtCursor(
+      document,
+      position
+    );
 
-    if (range) {
-      // check if character before word is a dot (.)
-      const lineText = document.lineAt(position).text;
-      const charIndex = range.start.character - 1;
-      if (charIndex >= 0 && lineText.charAt(charIndex) === ".") {
-        console.log("Hover request triggered after a dot");
+    const memberParts = memberAccessStringAtCursor.split(".");
 
-        return this.createMemberHover(range, word, lineText);
-      }
+    console.log(
+      `Hover requested for user code symbol: ${word}, memberAccessStringAtCursor: ${memberAccessStringAtCursor}`
+    );
+
+    if (memberParts.length > 1) {
+      // member access detected
+      return this.createMemberHover(memberParts);
     }
 
     // Check if it's a user function
@@ -63,98 +63,140 @@ export class UserCodeHoverProvider implements vscode.HoverProvider {
   }
 
   createMemberHover(
-    range: vscode.Range,
-    word: string,
-    lineText: string
+    memberParts: string[]
+  ): vscode.ProviderResult<vscode.Hover> {
+    // example: user hovers "member2" in "obj.member1.member2.member3"
+    // get first part
+    let word = memberParts.shift() || "";
+
+    console.log("Resolving member hover for word:", word);
+
+    // Check if it's a user variable
+    let userVariable = this.definitionProvider.findUserDefinedVariable(word);
+
+    if (!userVariable) {
+      console.log(
+        `Cannot resolve member access for part '${word}', stopping traversal.`
+      );
+      return null;
+    }
+
+    // console.log("Traversing member parts of variable:", userVariable);
+
+    while (memberParts.length > 0) {
+      let word = memberParts.shift() || "";
+      // console.log("LOOP: Resolving member definition for word:", word);
+
+      const variableType = userVariable.type;
+
+      // Check if the variable's type is a user-defined class
+      const userClass =
+        this.definitionProvider.findUserDefinedClass(variableType);
+
+      if (!userClass) {
+        console.log(
+          `Type '${variableType}' of variable '${userVariable.name}' is not a user-defined class, cannot resolve member '${word}'.`
+        );
+        return null;
+      }
+
+      // console.log(
+      //   `Looking for member '${word}' in class '${userClass.name}' members.`
+      // );
+
+      // Check if the member is a field
+      const classField = userClass.fields.find((field) => field.name === word);
+      if (classField) {
+        // console.log(
+        //   `Found field '${classField.name}' of class '${userClass.name}', continuing traversal.`
+        // );
+        userVariable = classField;
+
+        if (memberParts.length === 0) {
+          // Last part, return definition
+          return this.createUserClassFieldHover(classField);
+        }
+        continue;
+      }
+
+      // Check if the member is a method
+      const classMethod = userClass.methods.find(
+        (method) => method.name === word
+      );
+      if (classMethod) {
+        // console.log(
+        //   `Found method '${classMethod.name}' of class '${userClass.name}', continuing traversal.`
+        // );
+
+        if (memberParts.length === 0) {
+          // Last part, return definition
+          return this.createUserClassMethodHover(classMethod);
+        }
+        // For methods, we cannot continue traversal, so we stop here
+        return null;
+      }
+
+      console.log(
+        `Member '${word}' not found in class '${userClass.name}', stopping traversal.`
+      );
+      return null;
+    }
+
+    return null;
+  }
+
+  createUserClassMethodHover(
+    classMethod: UserFunction
   ): vscode.ProviderResult<vscode.Hover> {
     // Create markdown content
     const markdown = new vscode.MarkdownString();
     markdown.isTrusted = true;
 
-    // get token before the dot
-    const charIndex = range.start.character - 2;
-    let wordBeforeDot = "";
-    if (charIndex >= 0) {
-      const lineUpToDot = lineText.substring(0, charIndex + 1);
-      const words = lineUpToDot.match(/\b\w+$/);
-      if (words && words.length > 0) {
-        wordBeforeDot = words[0];
-      }
-    }
+    console.log(`Creating hover for class method: ${classMethod.name}`);
+    // Build method signature
+    const params = classMethod.arguments
+      .map((arg, index) => {
+        const isOptional = arg.optional;
+        const optionalMark = isOptional ? "?" : "";
+        return `${arg.name}${optionalMark}: ${arg.type}`;
+      })
+      .join(", ");
 
-    const userVariable: UserVariable | null =
-      this.definitionProvider.findUserDefinedVariable(wordBeforeDot || "");
+    const methodSignature = `${classMethod.name}(${params}): ${classMethod.returnType}`;
+    markdown.appendCodeblock(methodSignature, "leekscript");
 
-    if (userVariable) {
-      console.log(
-        `Providing member hover for variable: ${userVariable.name} of type: ${userVariable.type}`
-      );
+    markdown.appendMarkdown(
+      getMarkdownGoToDefinitionCommand(
+        classMethod.fileName,
+        classMethod.folderName,
+        classMethod.line,
+        classMethod.col
+      )
+    );
+    return new vscode.Hover(markdown);
+  }
 
-      // Check if the variable's type is a user-defined class
-      const userClass = this.definitionProvider.findUserDefinedClass(
-        userVariable.type
-      );
+  createUserClassFieldHover(
+    classField: UserVariable
+  ): vscode.ProviderResult<vscode.Hover> {
+    // Create markdown content
+    const markdown = new vscode.MarkdownString();
+    markdown.isTrusted = true;
 
-      if (userClass) {
-        console.log(
-          `Providing member hover for class: ${userClass.name} members`
-        );
-        // Check if the member is a field
-        const classField = userClass.fields.find(
-          (field) => field.name === word
-        );
-        if (classField) {
-          console.log(
-            `Creating hover for member field: ${classField.name} of class: ${userClass.name}`
-          );
-          const fieldDeclaration = `${classField.name}: ${classField.type}`;
-          markdown.appendCodeblock(fieldDeclaration, "leekscript");
+    console.log(`Creating hover for class field: ${classField.name}`);
+    const fieldDeclaration = `var ${classField.name}: ${classField.type}`;
 
-          markdown.appendMarkdown(
-            getMarkdownGoToDefinitionCommand(
-              classField.fileName,
-              classField.folderName,
-              classField.line,
-              classField.col
-            )
-          );
+    markdown.appendCodeblock(fieldDeclaration, "leekscript");
 
-          return new vscode.Hover(markdown);
-        }
-
-        // Check if the member is a method
-        const classMethod = userClass.methods.find(
-          (method) => method.name === word
-        );
-        if (classMethod) {
-          console.log(
-            `Creating hover for member method: ${classMethod.name} of class: ${userClass.name}`
-          );
-          // Build method signature
-          const params = classMethod.arguments
-            .map((arg, index) => {
-              const isOptional = arg.optional;
-              const optionalMark = isOptional ? "?" : "";
-              return `${arg.name}${optionalMark}: ${arg.type}`;
-            })
-            .join(", ");
-
-          const methodSignature = `${classMethod.name}(${params}): ${classMethod.returnType}`;
-          markdown.appendCodeblock(methodSignature, "leekscript");
-
-          markdown.appendMarkdown(
-            getMarkdownGoToDefinitionCommand(
-              classMethod.fileName,
-              classMethod.folderName,
-              classMethod.line,
-              classMethod.col
-            )
-          );
-
-          return new vscode.Hover(markdown);
-        }
-      }
-    }
+    markdown.appendMarkdown(
+      getMarkdownGoToDefinitionCommand(
+        classField.fileName,
+        classField.folderName,
+        classField.line,
+        classField.col
+      )
+    );
+    return new vscode.Hover(markdown);
   }
 
   createUserVariableHover(
